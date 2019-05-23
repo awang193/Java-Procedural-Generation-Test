8ab3a4c26ae5a926afa5e3deabe5a89ffb757fe7
Wang <1848389@00-63155.local>
2019-05-23 12:22:21 -0700

    Commit title

--- GUI.java
+++ GUI.java
@@ -1,23 +1,11 @@
-import SUPEROLDAPPROACH.DungeonTree;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class GUI extends Application
+
+public class GUI
 {
-    @Override
-    public void start(Stage stage) throws InterruptedException
-    {
-        BSPTree tree = new BSPTree(50, 50, 3);
-        System.gc();
-        System.exit(0);
-    }
-
     public static void main(String[] args)
     {
-        launch(args);
+        BSPTree tree = new BSPTree(50, 50, 3);
+        
+        
     }
-
-
-
-
 }
